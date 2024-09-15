95b8f2f190ec7ca8e2fc360f8f8ce69651fe6bcf
NamanVerma27 <namanv417@gmail.com>
2024-09-15 15:20:22 +0530

    Rec-basic

---
+++ digit_sum-rec.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int digitSum(int n){
+    if(n>=0 & n<=9) return n;
+    return digitSum(n/10) + n%10;
+}
+
+int main(){
+
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    cout<<"The sum of digits of the number: "<<digitSum(n)<<endl;
+
+    return 0;
+}
