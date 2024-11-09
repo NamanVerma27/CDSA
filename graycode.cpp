86a36b9afc97162d2b0f237deb94e1abdde26bff
NamanVerma27 <namanv417@gmail.com>
2024-11-09 21:30:22 +0530

    Rec_PW_A-5

--- graycode.cpp
+++ graycode.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-// Helper function to calculate the Gray code of the binary number
-int binaryToGray(int n) {
-    // Base case: when n is 0 or 1, return n (as MSB will be the same)
-    if (n == 0 || n == 1)
-        return n;
-    
-    // Recursively calculate the Gray code of n-1
-    int grayCode = binaryToGray(n / 2); // Recursively call for smaller values of n
-    
-    // The current Gray code is obtained by XORing n with the next bit of n
-    int currentGrayCode = (n % 2) ^ ((n / 2) % 2);
-    
-    // Return the updated Gray code
-    return currentGrayCode;
+int binary_to_gray(int n, int i = 0) {
+   // Base case when no digits left
+   if (n == 0) return 0;
+   
+   // Get last and second last digits
+   int curr = n % 10;
+   int next = (n / 10) % 10;
+   
+   // Calculate power of 10
+   int power = 1;
+   for(int j = 0; j < i; j++) {
+       power *= 10;
+   }
+   
+   // XOR current and next digit
+   int gray_digit = curr ^ next;
+   
+   // Recursive call for remaining digits and combine results
+   return binary_to_gray(n / 10, i + 1) + gray_digit * power;
 }
 
 int main() {
-    int n;
-    cout << "Enter the binary number in decimal: ";
-    cin >> n;
-    
-    cout << "The Gray code of " << n << " is: " << binaryToGray(n) << endl;
-    return 0;
+   int n;
+   cout << "Enter the binary number : ";
+   cin >> n;
+   cout << binary_to_gray(n) << endl;
+   return 0;
 }
